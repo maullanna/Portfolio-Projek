2bbfb6008214e9fa0e56b76fe5327bd0ee7686a1
maullanna <maullanna35@gmail.com>
2023-11-20 05:42:34 +0700

    update dark mode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -18,3 +23,53 @@ humberger.addEventListener("click", function () {
   humberger.classList.toggle("humberger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != humberger && e.target != navMenu) {
+    humberger.classList.remove("humberger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//darkmode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+//Animasi Teks
+const text = "I'm Yusuf Maulana"; // Teks yang akan diketik
+const speed = 300; // Kecepatan pengetikan (dalam milidetik)
+let index = 0;
+
+function typeWriter() {
+  const typedTextElement = document.getElementById("typedText");
+  const placeholderElement = document.querySelector(".placeholder");
+
+  if (index < text.length) {
+    typedTextElement.innerHTML += text.charAt(index);
+    index++;
+  } else {
+    index = 0;
+    typedTextElement.innerHTML = "";
+  }
+}
+
+// Memulai animasi pengetikan
+setInterval(typeWriter, speed);
+
+//pindahkan posisi toggle sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
